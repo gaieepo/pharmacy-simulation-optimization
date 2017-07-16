a70b2a73d02edf6d3da1e721a52f5164290f0415
gaieepo <gaieepo@gmail.com>
2017-07-17 02:48:16 +0800

    terminate when all done

--- test.py
+++ test.py
@@ -3,23 +3,15 @@ import random
 import itertools
 
 NUM_PHAR = 3
-SIM_TIME = 3600 * 9
-
-# SERVE_TIME = random.lognormvariate(4.8, 1.6) + random.lognormvariate(5.0, 1.6)
-# random.lognormvariate(4.8, 1.6) + random.lognormvariate(5.0, 1.6)
-
-# phar = Phar(env, NUM_PHAR)
-
-# class Phar:
-#     def __init__(self, env, num_phar):
-#         self.env = env
-#         self.phar_list = simpy.Resource(env, num_phar)
-
-#     def serve(self, patient):
-#         yield self.env.timeout(SERVE_TIME)
+SIM_TIME = 3600
 
 
-def patient(name, env, reg_phar, pac_phar, che_phar, dis_phar, pay_phar):
+class Phar(simpy.Resource):
+    def is_done(self):
+        return len(self.queue) == 0 and len(self.users) == 0
+
+
+def patient(name, env, test_event, reg_phar, pac_phar, che_phar, dis_phar, pay_phar):
     print('%s arrived at %.1f' % (name, env.now))
     with reg_phar.request() as req:
         yield req
@@ -46,26 +38,42 @@ def patient(name, env, reg_phar, pac_phar, che_phar, dis_phar, pay_phar):
         print('%s started pay at %.1f' % (name, env.now))
         yield env.timeout(random.lognormvariate(4.1, 1.2))
         print('%s finished pay at %.1f' % (name, env.now))
+    if env.now > SIM_TIME and is_all_done(reg_phar, pac_phar, che_phar, dis_phar, pay_phar):
+        test_event.succeed()
 
 
-def patient_generator(env, reg_phar, pac_phar, che_phar, dis_phar, pay_phar):
+def is_all_done(*args):
+    for arg in args:
+        if not arg.is_done():
+            return False
+    return True
+
+
+def patient_generator(env, test_event, reg_phar, pac_phar, che_phar, dis_phar, pay_phar):
+    total_time = 0
     for i in itertools.count():
-        env.process(patient('Patient %d' % i, env, reg_phar,
+        env.process(patient('Patient %d' % i, env, test_event, reg_phar,
                             pac_phar, che_phar, dis_phar, pay_phar))
-        yield env.timeout(random.expovariate(0.00798))
+        interval = random.expovariate(0.00798)
+        total_time += interval
+        if total_time > SIM_TIME:
+            break
+        yield env.timeout(interval)
+    yield test_event
 
 
 random.seed(42)
 env = simpy.Environment()
 
 # 2, 3, 1, 4, 1 = 11
-reg_phar = simpy.Resource(env, capacity=2)
-pac_phar = simpy.Resource(env, capacity=2)
-che_phar = simpy.Resource(env, capacity=2)
-dis_phar = simpy.Resource(env, capacity=3)
-pay_phar = simpy.Resource(env, capacity=2)
+reg_phar = Phar(env, capacity=2)
+pac_phar = Phar(env, capacity=2)
+che_phar = Phar(env, capacity=2)
+dis_phar = Phar(env, capacity=3)
+pay_phar = Phar(env, capacity=2)
 
 
-env.process(patient_generator(env, reg_phar,
-                              pac_phar, che_phar, dis_phar, pay_phar))
-env.run(until=SIM_TIME)
+test_event = env.event()
+p = env.process(patient_generator(env, test_event, reg_phar,
+                                  pac_phar, che_phar, dis_phar, pay_phar))
+env.run(until=p)
